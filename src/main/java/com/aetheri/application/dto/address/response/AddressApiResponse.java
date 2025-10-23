0a9d482f9347f529ada3d1bf772ea60b12b78164
Duskafka <rlarlgus0206@naver.com>
2025-10-23 14:10:27 +0900

    feat(#22): 도로명주소 API에 요청을 보냈을 때 응답을 파싱할 수 있는 Java `record` 구현
    임의로 필요한 값만 파싱하도록 함. 모든 값을 파싱한다면 파싱해야 하는 값이 너무 많아지고, 성능상 불이익을 볼 수 있기에 이렇게 구현함.

---
+++ src/main/java/com/aetheri/application/dto/address/response/AddressApiResponse.java
@@ -0,0 +1,9 @@
+package com.aetheri.application.dto.address.response;
+
+/**
+ * 주소 검증 요청
+ * */
+public record AddressApiResponse(
+        Results results
+) {
+}
---
+++ src/main/java/com/aetheri/application/dto/address/response/Results.java
@@ -0,0 +1,6 @@
+package com.aetheri.application.dto.address.response;
+
+public record Results(
+        Common common,
+        List<Juso> juso  // JSON 배열 "juso"는 List<Juso>로 매핑됩니다.
+) {}
