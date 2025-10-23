e879bc128979afa17d00da29939f46459d582a41
Duskafka <rlarlgus0206@naver.com>
2025-10-23 20:55:17 +0900

    test(#23): 테스트 검증 로직 개선
    - success_inquiry_address: 두 개의 주소가 받아와지는지 정확하게 검증하도록 함.
    - success_when_address_not_found: 존재하지 않는 주소를 요청해도 0개의 요청을 200 OK 응답이 오는지 테스트함.

--- src/test/java/com/aetheri/domain/adapter/out/address/AddressInquiryAdapterTest.java
+++ src/test/java/com/aetheri/domain/adapter/out/address/AddressInquiryAdapterTest.java
@@ -25,15 +25,16 @@ class AddressInquiryAdapterTest {
 
         // then
         StepVerifier.create(mono)
-                .expectNextMatches(response -> {
-                    // API 응답 구조 검증
-                    return response != null && response.results() != null;
-                })
+                .expectNextMatches(response -> response != null
+                        && response.results() != null
+                        && response.results().juso() != null
+                        && response.results().juso().size() == 2
+                )
                 .verifyComplete();  // 스트림이 성공적으로 완료되는지 검증 (이때까지 테스트가 대기함)
     }
 
     @Test
-    public void failure_when_executor_fails() {
+    public void success_when_address_not_found() {
         // given
         String address = "존재하지않는주소123456";
 
