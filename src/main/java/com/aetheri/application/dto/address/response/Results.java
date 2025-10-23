45222833f5b5e3b47e67551dedaefa492efd624f
Duskafka <rlarlgus0206@naver.com>
2025-10-23 14:11:31 +0900

    chore(#22): `List`를 `import`하지 않아서 컴파일 에러가 나는 것을 수정

--- src/main/java/com/aetheri/application/dto/address/response/Results.java
+++ src/main/java/com/aetheri/application/dto/address/response/Results.java
@@ -1,5 +1,7 @@
 package com.aetheri.application.dto.address.response;
 
+import java.util.List;
+
 public record Results(
         Common common,
         List<Juso> juso  // JSON 배열 "juso"는 List<Juso>로 매핑됩니다.
