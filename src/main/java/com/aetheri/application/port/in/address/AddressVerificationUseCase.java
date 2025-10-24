6f552d07a7dc9513112cf5c12afebe2974efcffa
Duskafka <rlarlgus0206@naver.com>
2025-10-24 18:37:06 +0900

    refactor(#17): `AddressVerificationUseCase` 클래스명 변경
    기존에 AddressVerificationPort였던 클래스명을 AddressVerificationUseCase로 변경함.
    
    - com.aetheri.application.port.in 디렉토리는 유즈케이스 접미사를 사용하는 규칙을 적용

--- src/main/java/com/aetheri/application/port/in/address/AddressVerificationUseCase.java
+++ src/main/java/com/aetheri/application/port/in/address/AddressVerificationUseCase.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Mono;
 /**
  * 도로명 주소 API 에게 요청을 보내기 위한 포트
  * */
-public interface AddressVerificationPort {
+public interface AddressVerificationUseCase {
     /**
      *
      * @param address 검증할 주소 문자열
